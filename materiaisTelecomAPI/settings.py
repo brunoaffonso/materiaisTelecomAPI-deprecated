41ec3cfaeb259717fc747f0614f8fc1ab25f8fcf
Bruno Affonso Teixeira <brunoaffonso@gmail.com>
2021-01-19 17:45:19 -0300

    testes delete servico

--- materiaisTelecomAPI/settings.py
+++ materiaisTelecomAPI/settings.py
@@ -65,18 +65,6 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
-
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://localhost",
